f179ecdcfae0c0e05f40516c6bf81c50b7621b02
Kacper Zimmer <kacperzimmer@outlook.com>
2026-07-12 19:23:32 +0200

    saving to database in the batches of 10

--- data_scraper/load_database.py
+++ data_scraper/load_database.py
@@ -1,4 +1,5 @@
 import feedparser
+import numpy as np
 import psycopg2
 import requests
 import trafilatura
@@ -6,70 +7,96 @@ import json
 
 from psycopg2._psycopg import cursor
 from trafilatura import extract
-from configparser import ConfigParser
-
 from data_scraper.test_sources import SOURCES, headers
 from analytics_engine.create_embeddings import (
     prepare_texts_for_embedding,
     generate_embeddings,
 )
 
+
+def save_data_to_postgres(embeddings_array: np.ndarray, article_list: list) -> None:
+    sql_to_insert_into_db = """
+    INSERT INTO embedded_articles (title, url, source, embedding) 
+    VALUES (%s, %s, %s, %s)
+    ON CONFLICT (url) DO NOTHING;
+    """
+
+    data_to_insert = []
+    embeddings_array_as_list = embeddings_array.tolist()
+
+    for idx in range(len(article_list)):
+        source_name = article_list[idx]['source_name']
+        title = article_list[idx]['title']
+        url = article_list[idx]['url']
+
+        single_embedding_for_article = embeddings_array_as_list[idx]
+
+        data_to_insert.append(
+            (title, url, source_name, single_embedding_for_article)
+        )
+
+    conn = None
+    try:
+        conn = psycopg2.connect(host='localhost', database='kontekst_db', user='newuser', password='password')
+        cur = conn.cursor()
+
+        cur.executemany(
+            sql_to_insert_into_db,
+            data_to_insert
+        )
+        conn.commit()
+        print(f"✅ Próba zapisu {len(data_to_insert)} artykułów zakończona (nowe zapisane, duplikaty pominięte).")
+
+    except psycopg2.OperationalError as e:
+        print(f"Błąd operacyjny {e}")
+    except psycopg2.ProgrammingError as e:
+        print(f"Błąd programistyczny {e}")
+    except Exception as e:
+        print(f'Blad ogolny {e}')
+    finally:
+        if conn:
+            conn.close()
+
+
+
+
+
+
 def create_init_db():
-      create_table_command = """
+    create_table_command = """
           CREATE TABLE IF NOT EXISTS embedded_articles(
               id bigserial primary key,
               title text,
+              url text UNIQUE,
               source text,
               embedding vector(1024)
           );
       """
-      conn = None
-      try:
-          conn = psycopg2.connect(
-              host='localhost',
-              database='kontekst_db',
-              user='newuser',
-              password='password'
-          )
-          cur = conn.cursor()
-          cur.execute(create_table_command)
-          cur.close()
-          conn.commit()
-
-          print("✅ Tabela embedded_articles utworzona pomyślnie.")
-      except psycopg2.OperationalError as e:
-          print(f"❌ Błąd połączenia z bazą: {e}")
-      except psycopg2.ProgrammingError as e:
-          print(f"❌ Błąd polecenia SQL: {e}")
-      except Exception as e:
-          print(f"❌ Nieoczekiwany błąd: {e}")
-      finally:
-          if conn:
-              conn.close()
-              print("🔌 Połączenie zamknięte.")
-
-
-
-def connect(config):
-    """ Connect to the PostgreSQL database server """
+    conn = None
     try:
-        with psycopg2.connect(**config) as conn:
-            print('Connected to the PostgreSQL server.')
-            return conn
-    except (psycopg2.DatabaseError, Exception) as error:
-        print(error)
+        conn = psycopg2.connect(
+            host='localhost',
+            database='kontekst_db',
+            user='newuser',
+            password='password'
+        )
+        cur = conn.cursor()
+        cur.execute(create_table_command)
+        cur.close()
+        conn.commit()
+
+        print("✅ Tabela embedded_articles utworzona pomyślnie.")
+    except Exception as e:
+        print(f"❌ Nieoczekiwany błąd podczas tworzenia tabeli: {e}")
+    finally:
+        if conn:
+            conn.close()
 
 
 def agg_news_artictles(data_news_companies_map):
     collected_data = []
 
     for source in data_news_companies_map:
-
-        if len(collected_data) >= 100:
-            clean_data = prepare_texts_for_embedding(collected_data)
-            generate_embeddings(clean_data)
-
-            collected_data = []
         if source.get('type') != 'rss':
             continue
 
@@ -82,11 +109,9 @@ def agg_news_artictles(data_news_companies_map):
 
         print(f"\nPrzeszukuję: {source['name']}...")
 
-
         for entry in feed.entries:
 
             print(f"  -> Ekstrakcja: {entry.link}")
-
             downloaded = trafilatura.fetch_url(entry.link)
 
             if not downloaded:
@@ -94,14 +119,12 @@ def agg_news_artictles(data_news_companies_map):
                 continue
 
             metadata = extract(downloaded, output_format="json", with_metadata=True)
-
             if not metadata:
                 print("     (Błąd ekstrakcji)")
                 continue
 
             data = json.loads(metadata)
             text = data.get('text')
-
             if not text:
                 print("(Brak treści w artykule)")
                 continue
@@ -115,16 +138,22 @@ def agg_news_artictles(data_news_companies_map):
                 "text_for_embedding": f"{data.get('title', entry.title)}. {text}"
             })
 
+            if len(collected_data) >= 10:
+                clean_data = prepare_texts_for_embedding(collected_data)
+                embeddings = generate_embeddings(clean_data)
+                save_data_to_postgres(embeddings, collected_data)
+
+                collected_data = []
+
+    if len(collected_data) > 0:
+        print(f"\nZapisuję ostatnią paczkę ({len(collected_data)} artykułów)...")
+        clean_data = prepare_texts_for_embedding(collected_data)
+        embeddings = generate_embeddings(clean_data)
+        save_data_to_postgres(embeddings, collected_data)
 
 
-
-    with open('articles.jsonl', 'a', encoding='utf-8') as f:
-        for article in collected_data:
-            f.write(json.dumps(article, ensure_ascii=False) + '\n')
-
-    print(f"\nSukces! Zapisano łącznie {len(collected_data)} artykułów do pliku 'articles.jsonl'.")
-
-
-# agg_news_artictles(SOURCES)  # uruchamiane przez main.py
 
 create_init_db()
+agg_news_artictles(SOURCES)  # uruchamiane przez main.py
+
+
