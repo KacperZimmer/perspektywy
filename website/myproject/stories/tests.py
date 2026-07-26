e5157f807d7bcc57f7a1ee26f848ac82fc2b9fa1
Kacper Zimmer <kacperzimmer@outlook.com>
2026-07-26 14:13:29 +0200

    test

--- website/myproject/stories/tests.py
+++ website/myproject/stories/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.db import connection
 
-# Create your tests here.
+
+class DatabaseConnection(TestCase):
+
+    def test_postgres_connection(self):
+        connection.ensure_connection()
+
+        print("\n--- DIAGNOSTYKA POŁĄCZENIA ---")
+        print("Silnik bazy (Vendor):", connection.vendor)
+        print("Użyta nazwa bazy:", connection.settings_dict['NAME'])
+        print("Użyty użytkownik:", connection.settings_dict['USER'])
+        print("------------------------------\n")
+
+        self.assertTrue(connection.is_usable(), "Brak połączenia z baza danych ")
