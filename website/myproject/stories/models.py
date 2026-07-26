42dfa99299b30bbb7dbd2d2d6627b77e0c50111f
Kacper Zimmer <kacperzimmer@outlook.com>
2026-07-26 15:34:17 +0200

    story model

--- website/myproject/stories/models.py
+++ website/myproject/stories/models.py
@@ -1,4 +1,24 @@
 from django.db import models
 
 
-# class Stories()
+class Clusters(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    centroid = models.TextField(blank=True, null=True)  # This field type is a guess.
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'clusters'
+
+
+class EmbeddedArticles(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    cluster = models.ForeignKey(Clusters, models.DO_NOTHING, blank=True, null=True)
+    title = models.TextField(blank=True, null=True)
+    url = models.TextField(unique=True, blank=True, null=True)
+    source = models.TextField(blank=True, null=True)
+    embedding = models.TextField(blank=True, null=True)  # This field type is a guess.
+    created_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'embedded_articles'
